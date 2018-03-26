f94e0dddd80e84e05a94a38d3f8f10e8a08b1bf7
Ayub Quadri <ayub.quadri@cyient-insights.com>
2018-03-26 16:29:17 +0530

    Motion detection using Background substraction method OpenCV

---
+++ MOG Background Reduction/Motion_detection.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import cv2
+
+video = cv2.VideoCapture(0)
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+	ret, frame = video.read()
+
+	fgmask = fgbg.apply(frame)
+
+	cv2.imshow('fgmask', frame)
+	cv2.imshow('frame', fgmask)
+
+	k = cv2.waitKey(30) & 0xff
+	if k ==27:
+		break
+
+video.release()
+cv2.destroyAllWindows()
